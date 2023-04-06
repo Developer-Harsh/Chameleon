ca57d7799949a0441f0ebbb9c0ed8db761f047a7
Dhruv Gupta <spdhruvgupta7861gmail.com>
2023-04-06 15:49:15 +0530

    SplashScreen Functionality

--- app/src/main/java/com/sneproj/chameleon/SplashActivity.java
+++ app/src/main/java/com/sneproj/chameleon/SplashActivity.java
@@ -2,13 +2,49 @@ package com.sneproj.chameleon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+
+import com.sneproj.chameleon.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
 
+    ActivitySplashBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().hide();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().setStatusBarColor(SplashActivity.this.getColor(R.color.white));
+            getWindow().setNavigationBarColor(SplashActivity.this.getColor(R.color.white));
+        }
+
+
+        Thread thread = new Thread(){
+
+            public void run(){
+                try {
+                    sleep(4000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+
+                }
+                finally {
+
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+
+                    startActivity(intent);
+                    finish();
+
+                }
+            }
+
+        };thread.start();
     }
-}
+
+    }
