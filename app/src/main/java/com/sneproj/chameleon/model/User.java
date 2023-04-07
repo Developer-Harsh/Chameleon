08ee1345b8bf1d8cb9e38d4989b369725f63a633
snevatechnologies <harsh95829@gmail.com>
2023-04-07 08:44:43 +0530

    added google auth in LoginActivity.java, created 2 java class first is Constants.java and second is User.java

---
+++ app/src/main/java/com/sneproj/chameleon/model/User.java
@@ -0,0 +1,7 @@
+package com.sneproj.chameleon.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    public String uid, profile, name, uname, bio, number, email, location, nativeLang, gender;
+}
---
+++ app/src/main/java/com/sneproj/chameleon/utils/Constants.java
@@ -0,0 +1,5 @@
+package com.sneproj.chameleon.utils;
+
+public class Constants {
+    public static final String COLLECTION_USERS = "users";
+}
