6387e24422d226edfae9f63cbd6c777ca4e04be0
Dhruv Gupta <spdhruvgupta7861gmail.com>
2023-04-07 23:58:58 +0530

    New user Activity xml

--- app/src/main/java/com/sneproj/chameleon/NewUserActivity.java
+++ app/src/main/java/com/sneproj/chameleon/NewUserActivity.java
@@ -2,13 +2,25 @@ package com.sneproj.chameleon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import com.sneproj.chameleon.databinding.ActivityNewUserBinding;
+
 public class NewUserActivity extends AppCompatActivity {
+    ActivityNewUserBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_user);
+        binding = ActivityNewUserBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().hide();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().setStatusBarColor(NewUserActivity.this.getColor(R.color.bg_main));
+            getWindow().setNavigationBarColor(NewUserActivity.this.getColor(R.color.bg_main));
+        }
+
     }
 }
